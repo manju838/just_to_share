3cbed29cbfabe5951f83268ac5e812f5f100e3da
Manjunadh Kandavalli <49728844+manju838@users.noreply.github.com>
2024-06-27 16:12:22 +0530

    Adds Box colliders recursively for scene

--- Assets/Scripts1/AddColliders.cs
+++ Assets/Scripts1/AddColliders.cs
@@ -1,55 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AddColliders : MonoBehaviour
 {
-    // Assign the root GameObject of the scene in the Inspector
-    public GameObject sceneRoot;
-
-    // Start is called before the first frame update
     void Start()
     {
-        if (sceneRoot != null)
-        {
-            AddCollidersToAllChildren(sceneRoot);
-        }
-        else
-        {
-            Debug.LogWarning("Scene root not assigned.");
-        }
+        AddCollidersToChildren(transform);
     }
 
-    void AddCollidersToAllChildren(GameObject root)
+    void AddCollidersToChildren(Transform parent)
     {
-        Renderer[] renderers = root.GetComponentsInChildren<Renderer>();
-
-        foreach (Renderer renderer in renderers)
+        foreach (Transform child in parent)
         {
-            GameObject obj = renderer.gameObject;
-
-            // Check if the object already has a collider
-            if (obj.GetComponent<Collider>() == null)
+            if (child.gameObject.GetComponent<Collider>() == null)
             {
-                // Add appropriate collider based on the renderer or mesh
-                if (renderer is MeshRenderer)
-                {
-                    obj.AddComponent<MeshCollider>().convex = true;
-                }
-                else if (renderer is SkinnedMeshRenderer)
-                {
-                    obj.AddComponent<MeshCollider>().convex = true;
-                }
-                else if (renderer is SpriteRenderer)
-                {
-                    obj.AddComponent<BoxCollider2D>();
-                }
-                else
-                {
-                    // Default to BoxCollider for any other type of renderer
-                    obj.AddComponent<BoxCollider>();
-                }
+                child.gameObject.AddComponent<BoxCollider>();
             }
+            // Recursively add colliders to the children
+            AddCollidersToChildren(child);
         }
     }
 }
