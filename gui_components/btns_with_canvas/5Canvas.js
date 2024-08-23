4cd566d74c697fde22cdf967fbad7a4795aea1a6
Manjunadh Kandavalli <49728844+manju838@users.noreply.github.com>
2024-08-23 16:17:02 +0530

    Update 5Canvas.js
    Completely commented segregated code

--- gui_components/btns_with_canvas/5Canvas.js
+++ gui_components/btns_with_canvas/5Canvas.js
@@ -1,35 +1,78 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Canvas.css';
 
-/* Drawing is working+ graph is added+pan+zoom working */
+/* Complete code with comments
+Pan+zoom+drawing lines(only continuous loops allowed)
+*/
 
-const Canvas = ({ isDrawing, isMoving, isDelete }) => {
+const Canvas = ({ isDrawing,  isMoving, isDelete}) => {
+
+    /*
+    useState(): Hook to add a state variable to your component.
+    useRef(): Hook to reference a value that’s not needed for rendering
+    useEffect(): Hook to synchronize a component with an external system.(control a non-React component based on the React state)
+
+    => If a React event object is returned by the eventhandler, it is a "Synthetic Event".(Eg: MouseEvent, DragEvent, KeyboardEvent, PointerEvent, TouchEvent)
+
+    1) https://react.dev/reference/react-dom/components/common#handling-pointer-events
+    2) https://www.w3schools.com/jsref/event_onmouseover.asp
+
+
+    The following are explanations for common mouseevents:
+
+    <div
+    onClick={e => console.log('Executes fn when clicked')}
+    onMouseEnter={e => console.log('Fires when the pointer moves inside an element')}
+    onMouseLeave={e => console.log('Fires when the pointer leaves an element')}
+    onMouseDown={e => console.log('Fires when the pointer is pressed down')}◘
+    onMouseUp={e => console.log('Fires when the pointer is released')}
+    onMouseOver={e => console.log('Fires when pointer is moved over an element')}
+    onDoubleClick={e=> console.log('Fires when pointer clicked twice')}
+    />
+    */
+
+    //////////////////////// State Management ////////////////////////
+
+    //////// Variable Type1: Canvas Reference ////////
     const canvasRef = useRef(null);
 
+    //////// Variable Type2: Panning ////////
     const [pan, setPan] = useState({ x: 0, y: 0 });
     const [isPanning, setIsPanning] = useState(false);
     const [startPos, setStartPos] = useState({ x: 0, y: 0 });
 
-    const [zoom, setZoom] = useState(1);
+    //////// Variable Type3: Zooming ////////
+    const [zoom, setZoom] = useState(1); // State to manage zoom level
 
-    const [dots, setDots] = useState([]);
-    const [lines, setLines] = useState([]);
+    const [dots, setDots] = useState([]); // State to manage dots
+    const [lines, setLines] = useState([]); // State to manage lines
+
     const [draggingDot, setDraggingDot] = useState(null);
     const [hoveredDot, setHoveredDot] = useState(null);
 
+    //////// Variable Type: Graph with all node and edge info ////////
     const [graph, setGraph] = useState({ nodes: {}, edges: [] });  // Initialize graph state
 
+    //////// Variable Type: Grid Definitions ////////
+    /* 
+    Meter version: Major grid divided into 10 minor grids 
+    */
     const majorGridSize = 100;
     const minorGridSize = majorGridSize / 10;
 
+    //////////////////////// Utility fns. ////////////////////////
+    
+    //////// Utility fns. Type1: Panning ////////
     const startPan = (e) => {
+        // Function to start panning
         if (!(isDrawing || isMoving || isDelete)) {  // Disable panning when any button is active
             setIsPanning(true);
-            setStartPos({ x: e.clientX - pan.x, y: e.clientY - pan.y });
+            setStartPos({ x: e.clientX - pan.x, y: e.clientY - pan.y }); //clientx and clienty are horizontal and vertical coordinate within the application's viewport (https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX)
         }
     };
 
     const panCanvas = (e) => {
+        // Function to pan the canvas
         if (isPanning) {
             setPan({
                 x: e.clientX - startPos.x,
@@ -39,29 +82,61 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
     };
 
     const endPan = () => {
+        // Function to end panning
         setIsPanning(false);
     };
 
+    //////// Utility fns. Type2: Zooming ////////
+    
     const handleZoom = (e) => {
-        e.preventDefault();
-        const zoomFactor = 1.1;
-        const newZoom = e.deltaY > 0 ? zoom / zoomFactor : zoom * zoomFactor;
-
+        // Function to handle zooming
+        e.preventDefault(); //stop the default scrolling behavior of the browser when the user scrolls, ensuring that the zoom action only affects your canvas.
+        const zoomFactor = 1.1; // Each scroll will increase or decrease the zoom by 10%.
+        const newZoom = e.deltaY > 0 ? zoom / zoomFactor : zoom * zoomFactor; // e.deltaY is vertical scroll amount, if it is greater than 0 then it is zoom out
+        
         if (newZoom >= 0.9 && newZoom <= 1.5) {
             setZoom(newZoom);
         }
     };
-
-    const calculateGridCoordinates = (x, y) => {
-        const adjustedX = (x - pan.x) / zoom;
-        const adjustedY = (y - pan.y) / zoom;
-
-        const snappedX = Math.round(adjustedX / minorGridSize) * minorGridSize;
-        const snappedY = Math.round(adjustedY / minorGridSize) * minorGridSize;
-
-        return { x: snappedX, y: snappedY };
+    
+    //////// Utility fns. Type3: Convert grid coordinates+draw lines+ draw dots(draw circle) ////////
+    
+    const toScreenCoordinates = (x, y) => {
+        //  Convert grid coordinates of a point (x, y) to the corresponding screen coordinates
+        // (x, y) are grid coordinates of point
+        return {
+            x: (x * zoom) + pan.x,
+            y: (y * zoom) + pan.y,
+        };
     };
-
+    
+    const drawLine = (ctx, startX, startY, endX, endY) => {
+        // Draw a line between two points on the canvas.
+        ctx.beginPath(); // Begins a new path or resets the current path.
+        ctx.moveTo(startX, startY); // Move starting point of the line to (startX, startY)
+        ctx.lineTo(endX, endY); // Draw a line to the endpoint coordinates
+        ctx.strokeStyle = 'black'; // Set color of line to black
+        ctx.lineWidth = 2 / zoom;
+        ctx.stroke(); // Strokes the line, making it visible.
+        ctx.closePath(); // Closes the path.
+    };
+    
+    const drawDot = (ctx, x, y, isHovered) => {
+        // Draws a dot at a specific point on the canvas
+        const dotSize = 2 / zoom;
+        
+        ctx.beginPath();
+        ctx.arc(x, y, dotSize, 0, 2 * Math.PI); // Draws an arc centered at (x, y) with radius dotSize and angle 2* Math.PI
+        ctx.fillStyle = isHovered ? 'red' : 'blue';  // Highlight dot if hovered
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2 / zoom;
+        ctx.fill(); // Fills the dot with the specified color.
+        ctx.stroke(); // Strokes the dot with the black outline.
+        ctx.closePath();
+    };
+    
+    //////// Utility fns. Type4: Add Wall Clicking ////////
+    
     const handleCanvasClick = (e) => {
         if (isDrawing) {
             const rect = canvasRef.current.getBoundingClientRect();
@@ -92,10 +167,29 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
         }
     };
 
-    const startDragDot = (index) => (e) => {
-        if (isDrawing) {
-            setDraggingDot(index);
+    const calculateGridCoordinates = (x, y) => {
+        const adjustedX = (x - pan.x) / zoom;
+        const adjustedY = (y - pan.y) / zoom;
+
+        const snappedX = Math.round(adjustedX / minorGridSize) * minorGridSize;
+        const snappedY = Math.round(adjustedY / minorGridSize) * minorGridSize;
+
+        return { x: snappedX, y: snappedY };
+    };
+
+    //////// Utility fns. Type5: Panning ////////
+
+    const handleMouseMove = (e) => {
+        if (isDrawing && draggingDot === null) {
+            const rect = canvasRef.current.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            const { x: gridX, y: gridY } = calculateGridCoordinates(x, y);
+            setHoveredDot(dots.findIndex(dot => dot.x === gridX && dot.y === gridY));
         }
+        dragDot(e);
+        panCanvas(e);
     };
 
     const dragDot = (e) => {
@@ -130,40 +224,36 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
         }
     };
 
-    const endDragDot = () => {
-        setDraggingDot(null);
-    };
+    //////////////////////// Handling Side Effects ////////////////////////
+    /*
+    The useEffect hook is used to perform side effects in your functional components, such as fetching data, subscribing to external events, or manually changing the DOM.
+    useEffect(() => {setup}, [optional dependencies]);
 
-    const handleMouseMove = (e) => {
-        if (isDrawing && draggingDot === null) {
-            const rect = canvasRef.current.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
+    Starter Tutorial: https://www.youtube.com/watch?v=-4XpG5_Lj_o
 
-            const { x: gridX, y: gridY } = calculateGridCoordinates(x, y);
-            setHoveredDot(dots.findIndex(dot => dot.x === gridX && dot.y === gridY));
-        }
-        dragDot(e);
-        panCanvas(e);
-    };
-
-    const toScreenCoordinates = (x, y) => {
-        return {
-            x: (x * zoom) + pan.x,
-            y: (y * zoom) + pan.y,
-        };
-    };
+    Note: Same file has multiple useEffect fns, managing different side effects
+    */
 
     useEffect(() => {
+        // Code in this curly braces gets executed once at beginning and whenever the dependency array is satisfied.
+
         const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d'); // returns a drawing context on the canvas (It can be "2d", "webgl", "webgl2", "webgpu", "bitmaprenderer")
 
         const drawGrid = () => {
+
+            /*
+            clearRect(left_pt_x_coordinate,left_pt_y_coordinate, width, height): erases the pixels in a rectangular area by setting them to transparent black.
+            strokestyle(left_pt_x_coordinate,left_pt_y_coordinate, width, height): specifies the color, gradient, or pattern to use for the strokes (outlines) around shapes
+            strokerect(left_pt_x_coordinate,left_pt_y_coordinate, width, height): draws a rectangle that is stroked (outlined) according to the current strokeStyle
+            */
+
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.save();
             ctx.scale(zoom, zoom);
             ctx.translate(pan.x / zoom % majorGridSize, pan.y / zoom % majorGridSize);
 
+            // Draw the canvas
             // Draw minor grid lines
             ctx.strokeStyle = '#e0e0e0';
             for (let x = -minorGridSize; x < canvas.width / zoom + minorGridSize; x += minorGridSize) {
@@ -181,7 +271,7 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
                 }
             }
 
-            ctx.restore();
+            ctx.restore(); // restores the state of a saved drawing context
 
             // Draw the lines
             lines.forEach(line => {
@@ -197,37 +287,20 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
             });
         };
 
-        const drawDot = (ctx, x, y, isHovered) => {
-            const dotSize = 2 / zoom;
-        
-            ctx.beginPath();
-            ctx.arc(x, y, dotSize, 0, 2 * Math.PI);
-            ctx.fillStyle = isHovered ? 'red' : 'blue';  // Highlight dot if hovered
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 2 / zoom;
-            ctx.fill();
-            ctx.stroke();
-            ctx.closePath();
-        };
-
-        const drawLine = (ctx, startX, startY, endX, endY) => {
-            ctx.beginPath();
-            ctx.moveTo(startX, startY);
-            ctx.lineTo(endX, endY);
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 2 / zoom;
-            ctx.stroke();
-            ctx.closePath();
-        };
-
+        // Function Calls
         drawGrid();
-    }, [pan, zoom, dots, lines, hoveredDot]);
+
+
+    },[pan, zoom, dots, lines, hoveredDot]);
 
     useEffect(() => {
+        // Graph is printed in console once Draw Wall is disabled
         if (!isDrawing && graph.edges.length > 0) {
             console.log("Graph:", graph);
         }
     }, [isDrawing, graph]);
+
+
 
     return (
         <canvas
@@ -236,13 +309,21 @@ const Canvas = ({ isDrawing, isMoving, isDelete }) => {
             width={window.innerWidth}
             height={window.innerHeight}
             onMouseDown={startPan}
-            onMouseMove={handleMouseMove}
             onMouseUp={endPan}
-            onWheel={handleZoom}
-            onClick={handleCanvasClick}
             onMouseLeave={endPan}
+            onWheel={handleZoom}
+            onMouseMove={handleMouseMove}
+            onClick={handleCanvasClick}
         />
     );
 };
 
 export default Canvas;
+
+/* 
+onClick={handleCanvasClick}    => Draw Wall link
+onWheel={handleZoom}           => Zooming link
+onMouseMove={handleMouseMove}  => Panning link
+
+Canvas "onMouseMove" is triggered whenever the mouse moves
+*/
